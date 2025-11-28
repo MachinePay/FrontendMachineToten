6df59db28766033dd804c71c66d68e34f3b23e58
Tuco2006 <mateussmcruz@icloud.com>
2025-11-28 12:09:02 -0300

    fix: sweetAlert

--- index.tsx
+++ index.tsx
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'sweetalert2/dist/sweetalert2.min.css';
 import App from './App';
 
 const rootElement = document.getElementById('root');
